30d63d058ce96478dd0e8f8bc2845200533e619b
thepenguinbird <chairguy983@gmail.com>
2025-07-18 13:14:56 +0200

    Delete shell directory
    to only focus on the logonui part of xfce-winxp-tc

--- shell/cpl/sysdm/src/intapi.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INTAPI_H__
-#define __INTAPI_H__
-
-//
-// PUBLIC CONSTANTS
-//
-#define WINTC_CPL_SYSDM_HOSTEXT_PAGE 1
-
-#endif
--- shell/taskband/src/taskband.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TASKBAND_H__
-#define __TASKBAND_H__
-
-#define TASKBAND_ROW_HEIGHT 30
-
-#endif
